039b44f6ab524460812ae7be7175ff703030543b
Tanguy BARTHELEMY <tangbarth@hotmail.fr>
2025-11-28 20:37:50 +0000

    first commit

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+init_dir="/home/onyxia/.cache/init"
+mkdir -p "${init_dir}"
+chown -R onyxia:users "${init_dir}"
+
+cd ${init_dir}
+echo "v1.14.1-gh-tractortom"
+
+curl -fsSL -O "https://gitlab.com/TractorTom/onyxia-setup/-/raw/main/library/setup-git-TractorTom.sh?ref_type=heads"
+curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-rstudio-prefs.sh"
+curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-locales.sh"
+curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-rprofile.sh"
+curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-air.sh"
+curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-firacode.sh"
+
+chmod +x "setup-git-TractorTom.sh"
+chmod +x "setup-rstudio-prefs.sh"
+chmod +x "setup-locales.sh"
+chmod +x "setup-rprofile.sh"
+chmod +x "setup-air.sh"
+chmod +x "setup-firacode.sh"
+
+"./setup-git-TractorTom.sh"
+"./setup-rstudio-prefs.sh"
+"./setup-locales.sh"
+"./setup-rprofile.sh"
+"./setup-air.sh"
+"./setup-firacode.sh"
---
+++ library/setup-git-TractorTom.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Préparer Git
+git config --global user.name "Tanguy BARTHELEMY"
+git config --global user.email tangbarth@hotmail.fr
+git config --global credential.username TractorTom
+git config --global credential.helper 'cache --timeout=604800'
+
+echo "GITHUB_PAT=\${GITHUB_TRACTORTOM}" >> "${HOME}/.Renviron"
